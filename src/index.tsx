6a89c49bd73d4257778ea295029f36e506d6b389
asalex04 <arc04@mail.ru>
2022-10-04 13:51:56 +0400

    add page loader, add not found page, add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import {render} from 'react-dom'
-import 'shared/config/i18n/i18n'
-import App from "./app/App";
-import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "app/providers/ThemeProvider";
+import { render } from 'react-dom';
+import 'shared/config/i18n/i18n';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import App from './app/App';
 
 render(
     <BrowserRouter>
-        <ThemeProvider>
-            <App/>
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </ErrorBoundary>
     </BrowserRouter>,
-    document.getElementById('root')
-)
+    document.getElementById('root'),
+);
