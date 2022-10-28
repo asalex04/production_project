4efe9e0078871c8237fdfa970d4d7807ab991f54
asalex04 <arc04@mail.ru>
2022-10-28 15:10:55 +0400

    add modal, portal, change header

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -9,13 +11,29 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
 
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
-
+            <Button
+                theme={ButtonTheme.CLEAR}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Enter')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Amet dictum sit amet justo donec enim diam vulputate. Enim sed faucibus
+                turpis in eu mi. Nibh cras pulvinar mattis nunc sed blandit libero.
+                Cras tincidunt lobortis feugiat vivamus at augue eget arcu.
+            </Modal>
         </div>
     );
 };
